94c1ff82802adbe99756ed9ec085e1d56e131095
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-15 01:05:17 -0300

    Revert "define each datapoint as a scrapy field"
    This reverts commit 3d0b17658199d9defc57ac611b79fb3565561454.

--- steam/items.py
+++ steam/items.py
@@ -7,12 +7,6 @@ import scrapy
 
 
 class SteamItem(scrapy.Item):
-    game_url = scrapy.Field()
-    image_url = scrapy.Field()
-    game_name = scrapy.Field()
-    release_date = scrapy.Field()
-    platform = scrapy.Field()
-    rating = scrapy.Field()
-    original_price = scrapy.Field()
-    discounted_price = scrapy.Field()
-    discount_rate = scrapy.Field()
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
