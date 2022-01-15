e55a17c5ad222047781e1920ca223e193e7836e7
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-15 19:48:56 -0300

    parse some items

--- steam/spiders/best_selling.py
+++ steam/spiders/best_selling.py
@@ -1,11 +1,19 @@
 import scrapy
 from ..items import SteamItem
 
-
 class BestSellingSpider(scrapy.Spider):
     name = 'best_selling'
     allowed_domains = ['store.steampowered.com']
     start_urls = ['https://store.steampowered.com/search/?filter=topsellers/']
-
+        
     def parse(self, response):
         steam_item = SteamItem()
+        games = response.xpath("//div[@id='search_resultsRows']/a")
+        for game in games:
+            steam_item['game_url'] = game.xpath(".//@href").get()
+            steam_item['image_url'] = game.xpath(".//div[@class='col search_capsule']/img/@src").get()
+            steam_item['game_name'] = game.xpath(".//span[@class='title']/text()").get()
+            steam_item['release_date'] = game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
+
+            yield steam_item
+            
