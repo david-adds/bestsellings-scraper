9131c48840ed195d804b48f84410cad649e66d30
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-15 20:01:57 -0300

    parse platform names

--- steam/spiders/best_selling.py
+++ steam/spiders/best_selling.py
@@ -1,5 +1,6 @@
 import scrapy
 from ..items import SteamItem
+from ..pipelines import get_platforms
 
 class BestSellingSpider(scrapy.Spider):
     name = 'best_selling'
@@ -14,6 +15,6 @@ class BestSellingSpider(scrapy.Spider):
             steam_item['image_url'] = game.xpath(".//div[@class='col search_capsule']/img/@src").get()
             steam_item['game_name'] = game.xpath(".//span[@class='title']/text()").get()
             steam_item['release_date'] = game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
-
+            steam_item['platforms'] = get_platforms(game.xpath(".//span[contains(@class,'platform_img') or @class='vr_supported']/@class").getall())
             yield steam_item
             
