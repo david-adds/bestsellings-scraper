9d18152cdce40458f00e652e0228c2141f0052d7
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-16 19:55:24 -0300

    pagination handling and reviews_summary issue treating

--- steam/spiders/best_selling.py
+++ steam/spiders/best_selling.py
@@ -1,7 +1,7 @@
 import scrapy
-from w3lib.html import remove_tags
 from ..items import SteamItem
-from ..pipelines import get_platforms, clean_discount_rate, get_original_price, clean_discounted_price
+from ..pipelines import get_platforms, clean_discount_rate
+from ..pipelines import get_original_price, clean_discounted_price, remove_html
 
 class BestSellingSpider(scrapy.Spider):
     name = 'best_selling'
@@ -17,10 +17,18 @@ class BestSellingSpider(scrapy.Spider):
             steam_item['game_name'] = game.xpath(".//span[@class='title']/text()").get()
             steam_item['release_date'] = game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
             steam_item['platforms'] = get_platforms(game.xpath(".//span[contains(@class,'platform_img') or @class='vr_supported']/@class").getall())
-            steam_item['reviews_summary'] = remove_tags(game.xpath(".//span[contains(@class,'search_review_summary')]/@data-tooltip-html").get())
+            steam_item['reviews_summary'] = remove_html(game.xpath(".//span[contains(@class,'search_review_summary')]/@data-tooltip-html").get())
             steam_item['discount_rate'] = clean_discount_rate(game.xpath(".//div[contains(@class,'search_discount')]/span/text()").get())
             steam_item['original_price'] = get_original_price(game.xpath(".//div[contains(@class,'search_price_discount_combined')]"))
             steam_item['discounted_price'] = clean_discounted_price(game.xpath("(.//div[contains(@class,'search_price discounted')]/text())[2]").get())
             yield steam_item
+            
+        next_page = response.xpath(".//a[@class='pagebtn' and text()='>']/@href").get()
+        if next_page:
+            yield scrapy.Request(
+                url=next_page,
+                callback=self.parse
+            )
+            
             
             
