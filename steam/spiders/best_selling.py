ceae590ec38e3e734a6db3c0c0c13d1761da93fc
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-16 16:40:16 -0300

    remove html tags

--- steam/spiders/best_selling.py
+++ steam/spiders/best_selling.py
@@ -7,7 +7,17 @@ class BestSellingSpider(scrapy.Spider):
     name = 'best_selling'
     allowed_domains = ['store.steampowered.com']
     start_urls = ['https://store.steampowered.com/search/?filter=topsellers/']
-        
+    
+    # def remove_html(self, review_summary):
+    #     cleaned_review_summary = ''
+    #     try:
+    #         cleaned_review_summary = remove_tags(review_summary)
+    #     except TypeError:
+    #         cleaned_review_summary = 'No reviews'
+            
+    #     return cleaned_review_summary
+
+            
     def parse(self, response):
         steam_item = SteamItem()
         games = response.xpath("//div[@id='search_resultsRows']/a")
@@ -17,6 +27,7 @@ class BestSellingSpider(scrapy.Spider):
             steam_item['game_name'] = game.xpath(".//span[@class='title']/text()").get()
             steam_item['release_date'] = game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
             steam_item['platforms'] = get_platforms(game.xpath(".//span[contains(@class,'platform_img') or @class='vr_supported']/@class").getall())
-            steam_item['reviews_summary'] = game.xpath(".//span[contains(@class,'search_review_summary')]/@data-tooltip-html").get()
+            steam_item['reviews_summary'] = remove_tags(game.xpath(".//span[contains(@class,'search_review_summary')]/@data-tooltip-html").get())
             yield steam_item
+            
             
