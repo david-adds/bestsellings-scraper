5b4405a59a286f346ee3ced9caf5d959878e3fb3
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-18 13:43:26 -0300

    undo pipeline file changes

--- steam/pipelines.py
+++ steam/pipelines.py
@@ -1,49 +1,13 @@
-from w3lib.html import remove_tags
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-def get_platforms(list_classes):
-    platforms = []
-    for item in list_classes:
-        platform = item.split(' ')[-1]
-        if platform=='win':
-            platforms.append('Windows')
-        elif platform=='mac':
-            platforms.append('Mac OS')
-        elif platform=='linux':
-            platforms.append('Linux')
-        # if platform=='vr_supported':
-        else:
-            platforms.append('VR Supported')
-    return platforms
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
 
 
-def remove_html(review_summary):
-    cleaned_review_summary = ''
-    try:
-        cleaned_review_summary = remove_tags(review_summary)
-    except TypeError:
-        cleaned_review_summary = 'No reviews'
-
-    return cleaned_review_summary
-
-
-def clean_discount_rate(discount_rate):
-    if discount_rate:
-        return discount_rate.lstrip('-')
-    return discount_rate
-
-
-def get_original_price(selector_obj):
-    original_price = ''
-    div_with_discount = selector_obj.xpath(".//div[contains(@class,'search_price discounted')]")
-    if len(div_with_discount) > 0:
-        original_price = div_with_discount.xpath(".//span/strike/text()").get()
-    else:
-        original_price = selector_obj.xpath("normalize-space(.//div[contains(@class,'search_price')]/text())").get()      
-    return original_price 
-
-
-def clean_discounted_price(discounted_price):
-    if discounted_price:
-        return discounted_price.strip()
-    return discounted_price
+class SteamPipeline:
+    def process_item(self, item, spider):
+        return item
